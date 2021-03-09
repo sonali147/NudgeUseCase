2dfd92390ace4087a8a32aa640ec8d8e4cdc0d41
Sonali Gupta <sonaligupta2510@gmail.com>
2021-03-09 12:49:18 +0530

    added tour plan funcion

--- run.py
+++ run.py
@@ -34,11 +34,11 @@ now = datetime.now()
 year = now.year
 year = 2017
 month = now.month
+#month = 4
 last_date = date(year, month+1,1) - timedelta(days=1)
 total_days_in_month = last_date.strftime("%d")
 month_date_list = [date(year,month,each).strftime("%Y-%m-%d") for each in range(1,int(total_days_in_month)+1)]
 #print(month_date_list)
-month = 4
 today_date = now.strftime("%Y-%m-%d")
 day_subtract_dict = {"Mon" : -5, "Tue" : -4, "Wed" : -3, "Thur" : -2, "Fri" : -1, "Sat" : 0, "Sun" : 1}
 
@@ -49,8 +49,9 @@ print(today_date)
 def main():
     division_wise_off = division_holidays()
     #print(division_wise_off)
-    id_list, summary = individual_summary(division_wise_off)
+    summary = individual_summary(division_wise_off)
     print(summary)
+    #mongo_dump(summary, "user_data", many=1)
     return True
 
 def create_db_conn():
@@ -121,7 +122,10 @@ def user_leaves(user_id):
     dates = []
     conn = create_db_conn()
     if conn is not None:
-        query = "SELECT * FROM `leaveapplication` left outer join leavestatus on leaveapplication.applno=leavestatus.applno where leaveapplication.emp_id={user_id} and leavestatus.approved!='Rejected' and leavestatus.approved!='Withdrawn' and year={year} and month={month}".format(user_id=user_id, year=year, month=month)
+        query = """SELECT * FROM `leaveapplication` 
+        left outer join leavestatus on leaveapplication.applno=leavestatus.applno 
+        where leaveapplication.emp_id={user_id} and leavestatus.approved!='Rejected' 
+        and leavestatus.approved!='Withdrawn' and year={year} and month={month}""".format(user_id=user_id, year=year, month=month)
         #for testing dates extraction if no of days leaves is more than 1 
         #query = "SELECT * FROM `leaveapplication` left outer join leavestatus on leaveapplication.applno=leavestatus.applno where leaveapplication.emp_id={user_id} and no_of_days>1".format(user_id=user_id)
         conn.execute(query)
@@ -135,6 +139,7 @@ def user_leaves(user_id):
                 dates.append(next_dt.strftime("%Y-%m-%d"))
                 next_dt = next_dt + timedelta(days=1) 
         #print("user leaves", dates, sep="\n")
+        dates = list(set(dates))
         conn.close()
     return dates
 
@@ -144,18 +149,19 @@ def get_dates(day_names):
     first_day = date(year, month, 1).isoweekday()
 
     def find_date(day_num):
-            final_date = 7*int(day_num) - first_day + sub
-            try:
-                date(year,month,final_date)
-                final_date = "{year}-{month}-{date}".format(year=year, month=month, date=final_date)
-            except ValueError:
-                final_date = None
-            return final_date
+        final_date = 7*int(day_num) - first_day + sub
+        try:
+            dt = date(year,month,final_date)
+            final_date = dt.strftime("%Y-%m-%d")
+        except ValueError:
+            final_date = None
+        return final_date
 
     for each in day_names:
         if regex.findall("\d", each):
             day = each[0:-1]
             day_num = each[-1]
+            day_num_list = []
         else:
             day = each
             day_num_list = [1,2,3,4,5]
@@ -179,19 +185,19 @@ def individual_summary(division_wise_off):
     conn = create_db_conn()
     if conn is not None:
         #remove limit 3 in prod
-        query = "select id, division, user_name, employeecode, monthdays, leaves, field_days as field_days_till_date ,tot_dr_calls , avg_dr_calls as call_avg , tot_dr_met , tot_chem_calls, avg_chem_calls from individual_mis_summary where month={month} and year={year} limit 3".format(month=month, year=year)
+        query = "select id, division, user_name, employeecode, monthdays, leaves, field_days as field_days_till_date ,tot_dr_calls , avg_dr_calls as call_avg , tot_dr_met , tot_chem_calls, avg_chem_calls from individual_mis_summary where month={month} and year={year} and id=6035 limit 3".format(month=month, year=year)
         #print(query)
         conn.execute(query)
         result = conn.fetchall()
         #print(result) #tuple
-        id_list = []
         for row in result:
             temp = deepcopy(row)
+            temp["year"] = year
+            temp["month"] = month 
             for k,v in temp.items():
                 if k in ["leaves", "field_days_till_date","tot_dr_calls","call_avg", "tot_dr_met", "tot_chem_calls",
                 "avg_chem_calls"]:
                     temp[k] = float(v)
-            id_list.append(row["id"])
             div = row["division"]
             if div in division_wise_off:
                 week_off = division_wise_off[div]
@@ -201,13 +207,15 @@ def individual_summary(division_wise_off):
             leaves = user_leaves(row["id"])
             temp["leaves"] = leaves
             temp["holidays"] = holidays
-            temp["field_days"] = sorted(list(set(month_date_list) - set(week_off) - set(holidays) - set(leaves)))
+            temp["field_days"] = sorted(list(set(month_date_list) - set(week_off.get("holiday_dates",[])) - set(holidays) - set(leaves)))
             temp["no_of_field_days"] = len(temp["field_days"])
             temp = adminmis(temp)
+            tp = tour_plan(row["id"])
             dcr_report = mongo_db["dcr"].find_one({"id": row["id"]}, {"_id": 0});
             if dcr_report:
                 dcr(dcr_report)
             else:
+                dcr_report = {}
                 dcr_report["id"] = row["id"]
                 dcr_report["dcr_open"] = []
                 dcr_report["dcr_completed"] = []
@@ -215,8 +223,9 @@ def individual_summary(division_wise_off):
                     dcr_report["dcr_open"].append({"date": dt, "status": "Open", "completed": None, "filedWithin": None})
                 dcr(dcr_report)
             data.append(temp)
+            mongo_dump(data, "user_data", many=1)
         conn.close()
-    return id_list, data
+    return data
 
 def adminmis(temp):
     result = {}
@@ -285,12 +294,59 @@ def dcr(dcr_report):
         result = conn.fetchall()
         for each in result:
             dt = each["date"]
+            dt_str = dt.strftime("%Y-%m-%d")
+            status = each["status"]
+            if status == "Pending":
+                if dt_str in dt_open:
+                    dcr_report["dcr_open"] = [each for each in dcr_report["dcr_open"] if each["date"] != dt_str ]
+                    filedWithin = (each["creation_date"] - dt).days
+                    dcr_report["dcr_completed"].append({"date":dt_str, "status":status, "completed_date":each["creation_date"].strftime("%Y-%m-%d"), "filedWithin":filedWithin})
+    mongo_dump(dcr_report, "dcr")
+
+def tour_plan(user_id):
+    result = ()
+    dr_details = {"id":user_id, "year": year, "month":month, "data":[]}
+    #checkIfInDb
+    response = mongo_db["tour_plan"].find_one({"id":user_id, "year":year, "month":month},{"_id":0})
+    if response and response.get("data",[]):
+        return response
+    conn = create_db_conn()
+    if conn is not None:
+        query1 = "Select crmid from touringyearmonth where year={year} and month={month} and smownerid={user_id}".format(year=year, month=month, user_id=user_id) 
+        conn.execute(query1)
+        result = conn.fetchone()
+        print(result)
+        if result.get("crmid", None):
+            crm_id = result["crmid"]
+            query2 = """SELECT touringdetails.patch_brick AS brickId, bricks.brickname AS brickName, 
+                        touringday.`day` AS `day`, contactdetails.contactid AS contactId, 
+                        trim(concat(trim(contactdetails.firstname),' ',trim(contactdetails.lastname))) AS contactName, 
+                        towns.station as station, towns.townid as townId,towns.townname as townName FROM touringday 
+                        INNER JOIN touringdetails ON touringdetails.id = touringday.daykey 
+                        inner join contact_wise_bricks on touringdetails.patch_brick=contact_wise_bricks.brick_id
+                        INNER JOIN touringcontacts ON touringcontacts.id = touringdetails.pid 
+                        INNER JOIN contactdetails ON contactdetails.contactid = touringcontacts.contactid
+                        inner join bricks on contact_wise_bricks.brick_id=bricks.brickid 
+                        INNER JOIN townbrickassociation ON contactdetails.brick = townbrickassociation.brickid 
+                        INNER JOIN towns ON townbrickassociation.townid = towns.townid 
+                        INNER JOIN touringyearmonth ON touringday.crmid = touringyearmonth.crmid 
+                        WHERE touringcontacts.contacttype = 'DR' and contactdetails.deleted=0 and bricks.deleted=0 
+                        and contactdetails.brick=touringdetails.patch_brick and contact_wise_bricks.deleted=0 AND touringyearmonth.crmid = {crm_id}  
+                        GROUP BY contactdetails.contactid""".format(crm_id=crm_id)
+            conn.execute(query2)
+            drs_in_tp = conn.fetchall()
+            if drs_in_tp:
+                data = []
+                for row in drs_in_tp:
+                    data.append(row)
+                dr_details["data"]= data
+                mongo_dump(dr_details, "tour_plan")
+            conn.close()
+    return dr_details
 
 def ach_percent():
     query = ""
 
-def two_v_coverage():
-    query = ""
 
 if __name__=="__main__":
     print("Working...")
@@ -299,4 +355,5 @@ if __name__=="__main__":
     #print(get_dates(["Fri2", "Mon3", "Sun"]))
     #print(user_holiday(864))
     #print(user_leaves(864))
+    #print(tour_plan(6035))
 
